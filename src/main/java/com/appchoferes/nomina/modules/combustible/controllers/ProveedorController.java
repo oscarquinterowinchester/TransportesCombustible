df88f02e7ff972bd40c935d174cea42d14c7fe12
Drago2527 <erick.acosta@uabc.edu.mx>
2025-01-03 13:44:53 -0800

    se regreso a la configuracion de datacontex en controllers de combustible

--- src/main/java/com/appchoferes/nomina/modules/combustible/controllers/ProveedorController.java
+++ src/main/java/com/appchoferes/nomina/modules/combustible/controllers/ProveedorController.java
@@ -5,14 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.appchoferes.nomina.modules.combustible.dtos.ProveedorDTO;
 import com.appchoferes.nomina.modules.combustible.services.ProveedorService;
-import com.appchoferes.nomina.security.dtos.LoginDTO;
-import com.appchoferes.nomina.security.dtos.UsuarioDBDTO;
-import com.appchoferes.nomina.security.services.UsuarioDBService;
 
 @RestController
 @RequestMapping("/api/combustible")
@@ -21,14 +17,9 @@ public class ProveedorController {
     @Autowired
     private ProveedorService proveedorService;
 
-    @Autowired
-    private UsuarioDBService usuarioDBService;
-
     @GetMapping("/proveedores")
-    public List<ProveedorDTO> getProveedores(@RequestParam String username) {
-        LoginDTO loginDTO = new LoginDTO(username, "password_placeholder");
-        UsuarioDBDTO usuarioAutenticado = usuarioDBService.authenticationUsuario(loginDTO);
-        String databaseName = usuarioAutenticado.getDatabase_name();
+    public List<ProveedorDTO> getProveedores() {
+        String databaseName = "lorasdb";
         return proveedorService.getProveedores(databaseName);
     }
 }
