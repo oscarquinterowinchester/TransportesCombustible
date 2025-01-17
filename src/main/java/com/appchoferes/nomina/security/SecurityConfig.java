23ac2f8ca7f513bbff6e25a6624cbdf68ea44d5d
Drago2527 <erick.acosta@uabc.edu.mx>
2025-01-16 17:22:30 -0800

    token, revisar credenciales de usuario en base de datos

--- src/main/java/com/appchoferes/nomina/security/SecurityConfig.java
+++ src/main/java/com/appchoferes/nomina/security/SecurityConfig.java
@@ -5,11 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 import jakarta.servlet.http.HttpServletResponse;
 
-import org.springframework.security.web.AuthenticationEntryPoint;
 @Configuration
 public class SecurityConfig {
 
