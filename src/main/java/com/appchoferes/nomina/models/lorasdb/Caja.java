6f128b931065d158b0ff50483776abbc0694a000
Drago2527 <erick.acosta@uabc.edu.mx>
2025-01-17 16:57:46 -0800

    correccion de insercion de carga para camion, revisar para caja

--- src/main/java/com/appchoferes/nomina/models/lorasdb/Caja.java
+++ src/main/java/com/appchoferes/nomina/models/lorasdb/Caja.java
@@ -37,7 +37,7 @@ public class Caja {
     @Column(name = "Tanques")
     private double tanqueCapacidad;
 
-    @OneToMany(mappedBy = "caja", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    private List<CargaDiesel> cargas;
+    /*@OneToMany(mappedBy = "caja", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<CargaDiesel> cargas;*/
 
 }
